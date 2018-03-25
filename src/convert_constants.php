a12bb2bbb0d50a99fdc8c3f3498ecb79b9734c21
Ben Peachey <potherca@gmail.com>
2018-03-25 19:32:24 +0000

    Fixes bug caused by incorrect type casting.

--- src/convert_constants.php
+++ src/convert_constants.php
@@ -10,11 +10,13 @@ namespace Potherca\PlantUmlSkinparamParser;
  */
 function convert_constants(array $skinParams, array $substitutes) {
     array_walk($skinParams, function (&$value, $key) use ($substitutes) {
-        if (is_string($value) && array_key_exists($value, $substitutes)) {
+        if ($value === null) {
+            $value = 'null';
+        } else if (is_bool($value)) {
+            $value = $value?'true':'false';
+        } else if (is_string($value) && array_key_exists($value, $substitutes)) {
             $value = $substitutes[$value];
-        }
-
-        if (preg_match('/^[A-Z]+$/', $value) === 1) {
+        } else if (preg_match('/^[A-Z]+$/', $value) === 1) {
             $value = strtolower($value);
         }
     });
